a99c52241c44a4a2838d93dd3852865fab16b799
Vadim Vaskevich <nilon@list.ru>
2020-12-19 06:05:13 +0300

    add Home Work 6

---
+++ HomeWork6.cs
@@ -0,0 +1,28 @@
+using System;
+namespace ijuniorEducationHomework
+{
+    public class HomeWork6
+    {
+        public static void Work()
+        {
+            string nameUser;
+            string ageUser;
+            string zodiacUser;
+            string placeWorkUser;
+
+            Console.WriteLine("Как вас зовут?");
+            nameUser = Console.ReadLine();
+
+            Console.WriteLine("Сколько вам лет?");
+            ageUser = Console.ReadLine();
+
+            Console.WriteLine("Какой ваш знак зодиака?");
+            zodiacUser = Console.ReadLine();
+
+            Console.WriteLine("Где Вы работаете?");
+            placeWorkUser = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {nameUser}, вам {ageUser} год, Вы {zodiacUser} и работаете на {placeWorkUser}.");
+        }
+    }
+}
