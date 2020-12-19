ba6343b1d4cb6632c078e80cd3d83c9f2388ef08
Vadim Vaskevich <nilon@list.ru>
2020-12-19 06:03:39 +0300

    add Home Work 5

---
+++ HomeWork5.cs
@@ -0,0 +1,17 @@
+using System;
+namespace ijuniorEducationHomework
+{
+    public class HomeWork5
+    {
+        public static void Work()
+        {
+            int countPeople;
+            int timeMinuteInspection = 10;
+
+            Console.WriteLine("Введите количество людей в очереди:");
+            countPeople = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Вы должны отстоять в очереди {(countPeople * timeMinuteInspection) / 60} часа и {(countPeople * timeMinuteInspection) % 60} минут.");
+        }
+    }
+}
