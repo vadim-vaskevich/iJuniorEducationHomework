afcb1fd16a34064aeb5c22717e11d5eca680a760
Vadim Vaskevich <nilon@list.ru>
2020-12-19 05:39:44 +0300

    add Home Work 1

---
+++ iJuniorHomeWork1.cs
@@ -0,0 +1,16 @@
+namespace ijuniorEducationHomework
+{
+    public class iJuniorHomeWork1
+    {
+        byte levelUser;
+        sbyte armorUser;
+        short damageUser;
+        ushort criticalDamageUser;
+        int healthUser;
+        uint countOfStars;
+        long distanceToHouse;
+        ulong childrenOnEarth;
+        float moneyInWallet;
+        double hairLength;
+    }
+}
