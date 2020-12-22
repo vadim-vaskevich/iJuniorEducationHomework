0c7ef314a184e592b69c56a481594709c66a4d08
Vadim Vaskevich <nilon@list.ru>
2020-12-22 19:00:18 +0300

    add HomeWork18

--- HomeWork18.cs
+++ HomeWork18.cs
@@ -1,22 +1,58 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace ijuniorEducationHomework
 {
     public class HomeWork18
     {
-        /*
-        Пользователь вводит числа, и программа их запоминает.
-        Как только пользователь введёт команду sum, программа выведет сумму всех веденных чисел.
-        Выход из программы должен происходить только в том случае, если пользователь введет команду exit.
-        Программа должна работать на основе расширения массива.
-        Внимание, нельзя использовать List<T> и Array.Resize
-         */
         public static void Work()
         {
-            Console.WriteLine("Меню:");
-            Console.WriteLine("Введите число\n");
-            Console.WriteLine("Ввведите sum для подсчета введенных чисел\n");
-            Console.WriteLine("Введите exit - выход из программы\n");
+            string inputString = null;
+            int[] firstArray = new int[0];
+            int[] secondArray;
+            int amount;
+
+            while (inputString != "exit")
+            {
+                Console.WriteLine("Меню:");
+                Console.WriteLine("Введите число");
+                Console.WriteLine("Ввведите sum для подсчета введенных чисел");
+                Console.WriteLine("Введите exit - выход из программы");
+
+                inputString = Console.ReadLine();
+
+                switch (inputString)
+                {
+                    case "sum":
+                        amount = 0;
+
+                        for (int i = 0; i < firstArray.Length; i++)
+                            amount += firstArray[i];
+                        Console.Clear();
+                        Console.WriteLine($"Сумма введённых чисел равна {amount}");
+
+                        break;
+
+                    case "exit":
+                        Console.WriteLine("Выход из программы...");
+                        break;
+
+                    default:
+                        int inputNumber = int.Parse(inputString);
+
+                        secondArray = new int[firstArray.Length + 1];
+                        secondArray[secondArray.Length - 1] = inputNumber;
+
+                        for (int i = 0; i < firstArray.Length; i++)
+                            secondArray[i] = firstArray[i];
+
+                        firstArray = secondArray;
+                        Console.Clear();
+                        Console.WriteLine($"Размер массива: {firstArray.Length}");
+
+                        break;
+                }
+            }
         }
     }
 }
